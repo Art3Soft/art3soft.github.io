4f5f1a0fa778ca60d2b520cafb0100ac00409ed5
Daniel M. Sanchez <dmsanchez86@misena.edu.co>
2016-05-09 11:39:02 -0500

    menu de navegacion completo, se añadieron nuevos archivos a ignorar, cambios al diseño de la pagina, nuevos eventos

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 window.onload = function(){
 	console.log("------------------- Art 3 Soft -------------------");
 	
+	// evento del boton del menu
 	$(".btnMenu").click(function(){
 		$(this).toggleClass("active");
+	});
+	
+	// evento de los links del menu
+	$(".nav a").click(function(){
+		$(".nav a").removeClass("active");
+		$(this).addClass("active");
 	});
 }
